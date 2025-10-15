00733aa8bfcb6e21ff093c5050afc7bb6f71b2f6
Areen-Zainab <areenzainab66@gmail.com>
2025-10-15 08:58:28 +0500

    Database connected with backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,17 +1,23 @@
 const express = require("express");
 const cors = require("cors");
-const dotenv = require("dotenv");
+require("dotenv").config();
 
-dotenv.config();
+const pool = require("./config/db");
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Backend server is running ");
+// Test Route
+app.get("/api/test", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+    res.json({ message: "Connected ", time: result.rows[0] });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Database connection failed" });
+  }
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(` Server running on http://localhost:${PORT}`));
